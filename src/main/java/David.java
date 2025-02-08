1b298107983d65a8a0b10c75465b4c0334de42dc
Gyurim <gyurimkimyok@gmail.com>
2025-02-08 22:04:22 +0800

    Level 5. Handle Errors

--- src/main/java/David.java
+++ src/main/java/David.java
@@ -18,37 +18,44 @@ public class David {
         int i = 0;
         char input_type = input.charAt(input.indexOf("[") + 1);
         while (!input.equals("bye")) {
-            if (input.equals("list")) {
-                System.out.println(LINE_SEPERATOR + "\n" + "Here are the tasks in your list:\n");
-                for (int j = 0; j < i; j++) {
-                    System.out.println((j + 1) + "." + task[j].toString() + "\n" + LINE_SEPERATOR);
+            try {
+                if (input.equals("list")) {
+                    System.out.println(LINE_SEPERATOR + "\n" + "Here are the tasks in your list:\n");
+                    for (int j = 0; j < i; j++) {
+                        System.out.println((j + 1) + "." + task[j].toString() + "\n" + LINE_SEPERATOR);
+                    }
+                } else if (input.startsWith("mark")) {
+                    markTask(task, Integer.parseInt(input.split(" ")[1]) - 1);
+                    printMark(task, Integer.parseInt(input.split(" ")[1]) - 1);
+
+                } else if (input.startsWith("unmark")) {
+                    unmarkTask(task, Integer.parseInt(input.split(" ")[1]) - 1);
+                    printUnmark(task, Integer.parseInt(input.split(" ")[1]) - 1);
+                } else if (input.startsWith("todo")) {
+                    task[i] = new Todo(input.substring(getIndex(input, " ") + 1));
+                    System.out.println(printTaskType(task, i));
+                    i++;
+                } else if (input.startsWith("deadline")) {
+                    task[i] = new Deadline((input.substring(getIndex(input, " ") + 1, getIndex(input, "/") - 1)), input.substring(getIndex(input, "by") + 3));
+                    System.out.println(printTaskType(task, i));
+                    i++;
+                } else if (input.startsWith("event")) {
+                    task[i] = new Event((input.substring(getIndex(input, " ") + 1, getIndex(input, "/") - 1)), input.substring(getIndex(input, "from") + 5, getIndex(input, "to") - 2), input.substring(getIndex(input, "to") + 3));
+                    System.out.println(printTaskType(task, i));
+                    i++;
+
+                } else {
+//                System.out.println(LINE_SEPERATOR + "\n"
+//                        + "added: " + input + "\n" + LINE_SEPERATOR);
+//                task[i] = new Task(input);
+//                i++;
+                    throw new IllegalArgumentException(LINE_SEPERATOR + System.lineSeparator() + " Sorry... I don't know what you mean by that? Could you try again?" + System.lineSeparator() + LINE_SEPERATOR);
                 }
-            } else if (input.startsWith("mark")) {
-                markTask(task, Integer.parseInt(input.split(" ")[1]) - 1);
-                printMark(task, Integer.parseInt(input.split(" ")[1]) - 1);
-
-            } else if (input.startsWith("unmark")) {
-                unmarkTask(task, Integer.parseInt(input.split(" ")[1]) - 1);
-                printUnmark(task, Integer.parseInt(input.split(" ")[1]) - 1);
-            } else if (input.startsWith("todo")) {
-                task[i] = new Todo(input.substring(getIndex(input, " ") + 1));
-                System.out.println(printTaskType(task, i));
-                i++;
-            } else if (input.startsWith("deadline")) {
-                task[i] = new Deadline((input.substring(getIndex(input, " ") + 1, getIndex(input, "/") - 1)), input.substring(getIndex(input, "by") + 3));
-                System.out.println(printTaskType(task, i));
-                i++;
-            } else if (input.startsWith("event")) {
-                task[i] = new Event((input.substring(getIndex(input, " ") + 1, getIndex(input, "/") - 1)), input.substring(getIndex(input, "from") + 5, getIndex(input, "to") - 2), input.substring(getIndex(input, "to") + 3));
-                System.out.println(printTaskType(task, i));
-                i++;
-
-            } else {
-                System.out.println(LINE_SEPERATOR + "\n"
-                        + "added: " + input + "\n" + LINE_SEPERATOR);
-                task[i] = new Task(input);
-                i++;
             }
+         catch (IllegalArgumentException e) {
+            // Print the custom error message without stack trace
+            System.out.println(e.getMessage());
+        }
             input = scanner.nextLine();
 
         }
@@ -92,8 +99,7 @@ public class David {
     public static int getIndex(String marking, String find) {
         int index = marking.indexOf(find);
         if (index == -1) {
-            System.out.println("Error: Could not find space in the input.");
-            return -1;
+            throw new IllegalArgumentException(LINE_SEPERATOR + System.lineSeparator() + " Oops! The description of a task should NOT be blank! Please try again :)" + System.lineSeparator() + LINE_SEPERATOR);
         }
         return index;
     }
